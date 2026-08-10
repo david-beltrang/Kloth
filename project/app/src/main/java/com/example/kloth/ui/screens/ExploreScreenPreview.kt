d43e85859c2520341ade13cde85b0410556c41d2
David Beltrán. <estedavid0104@gmail.com>
2026-08-10 10:08:35 -0500

    feat: add preview for ExploreScreen component

---
+++ project/app/src/main/java/com/example/kloth/ui/screens/ExploreScreenPreview.kt
@@ -0,0 +1,10 @@
+package com.example.kloth.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true, device = "id:pixel_5")
+@Composable
+private fun ExploreScreenPreview() {
+    ExploreScreen()
+}
