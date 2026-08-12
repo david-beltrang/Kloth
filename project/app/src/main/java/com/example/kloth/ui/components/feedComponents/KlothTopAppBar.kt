f0fa609d9e078adbfed83f9b3da0f95ba03a6be7
LOUWPII <148557091+LOUWPII@users.noreply.github.com>
2026-08-11 23:02:13 -0500

    Feature/explore screen (#2)
    * Se ha implementado la pantalla del Feed (feedFollowing), principalmente se crearon unos componentes.kt modeluares con la finalidad de mantener el proyecto limpio y simplemente importarlos desde el composable principal. Estoscomponentes incluyen FeedTabRow, el cual permite seleccionar entre following y for you. KlothBadge.kt el cual permite crear una etiqueta, KlothTopAppBar el cual tiene la barra de más arriba con el titulo, las notificaciones y la lupa de busqueda, PostCard - la cual contiene toda la publicacion como tal y un KlothBottomNavigation.kt el cual permite navegar por las pantallas mas importantes de la aplicacion. Tambien se creó una carpeta theme que contiene Color.kt en el cual se define la paleta de colores, Theme.kt para la distribucion de páleta de colores, Type.kt para poder administrar el tamaño de las letras. Todo se reune en un composable principal que estan el archivo FeedScreen.kt
    
    * feat: add .gitignore files for project and app directories
    
    * feat: add .kotlin/ to .gitignore for improved build cleanliness
    
    * feat: add gradle.properties and gradlew for project configuration
    
    * feat: add initial Gradle wrapper and configuration files
    
    * feat: add initial Gradle configuration and settings for the Kloth project
    
    * feat: add initial resource files for colors, strings, themes, and backup rules
    
    * feat: add AndroidManifest.xml for application configuration
    
    * feat: implement MainActivity with ExploreScreen and edge-to-edge support
    
    * feat: add BottomBar component with navigation items and styling
    
    * feat: add TopAppBar component with back and search functionality
    
    * feat: add adaptive launcher icons and vector drawables for app branding
    
    * feat: add CatalogCard component for product display with image and favorite functionality
    
    * feat: add CategoryChip component for selectable category display
    
    * feat: add FilterChip component for customizable filter options
    
    * feat: add SearchBar component for user search functionality
    
    * feat: implement ExploreScreen with category selection and product display
    
    * feat: add preview for ExploreScreen component
    
    * feat: add Color.kt for theme color definitions
    
    * feat: add Theme.kt and Type.kt for theming support
    
    * feat: add new drawable assets for product images
    
    * feat: add data extraction rules XML file for backup configuration
    
    * feat: add comment to clarify the purpose of the AndroidManifest.xml
    
    * feat: add comments to clarify purpose of various components and resources
    
    * feat: remove ExploreScreen and ExploreScreenPreview components
    
    * feat: update dependencies for navigation and image loading
    
    * feat: add ExploreScreen and ExploreScreenPreview components
    
    * feat: add Product data class to represent product details
    
    * feat: implement ItemDetailScreen with HeroImage, GeneralInfo, and Ratings sections
    
    * feat: add additional color definitions for theme customization
    
    * feat: add navigationCompose version and library reference
    
    * feat: add navigation graph for Explore and ItemDetail screens
    
    * feat: update BottomBar to use painter resources for icons and remove unused imports in TopAppBar
    
    * feat: refactor MainActivity to set up navigation graph and remove ExploreScreen preview
    
    * feat: add new drawable resources for create, home, notification, profile, and search icons
    
    * Composable de notificacion creado para reutilizarlo
    
    ---------
    
    Co-authored-by: David Beltrán. <estedavid0104@gmail.com>

---
+++ project/app/src/main/java/com/example/kloth/ui/components/feedComponents/KlothTopAppBar.kt
@@ -0,0 +1,100 @@
+package com.example.kloth.ui.components.feedComponents
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.kloth.R
+import com.example.kloth.ui.theme.KlothTheme
+import com.example.kloth.ui.theme.RedInferno
+
+/*
+Este composable se usa en la pantalla FeedScreen y funciona como barra de navegacion.
+Es de tipo CenterAlignedTopAppBar, Su característica es que el título de la pantalla se alinea de forma automática en el centro horizontal de la barra
+
+link de referencia: https://kotlinlang.org/api/compose-multiplatform/material3/androidx.compose.material3/-center-aligned-top-app-bar.html
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KlothTopAppBar(
+    modifier: Modifier = Modifier,
+    notificationCount: Int = 0
+
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier.height(100.dp),
+        title = {
+            Image(
+                painter = painterResource(id = R.drawable.klogo),
+                contentDescription = stringResource(id = R.string.app_name),
+                modifier = Modifier.size(130.dp)
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* TODO */ }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = stringResource(id = R.string.content_desc_back)
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* TODO */ }) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(id = R.string.content_desc_search)
+                )
+            }
+            BadgedBox(
+                badge = {
+                    if (notificationCount > 0) {
+                        Badge(
+                            containerColor = RedInferno,
+                            contentColor = Color.White,
+                            modifier = Modifier
+                                .size(14.dp)
+                                .offset(x = (-4).dp, y = 4.dp) // Movemos el badge hacia adentro
+                        ) {
+                            Text(
+                                text = notificationCount.toString(),
+                                fontSize = 9.sp
+                            )
+                        }
+                    }
+                }
+            ) {
+                IconButton(onClick = { /* TODO */ }) {
+                    Icon(
+                        imageVector = Icons.Default.Notifications,
+                        contentDescription = stringResource(id = R.string.content_desc_notifications)
+                    )
+                }
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
+    )
+}
+
+@Preview
+@Composable
+fun KlothTopAppBarPreview() {
+    KlothTheme {
+        KlothTopAppBar(notificationCount = 5)
+    }
+}
