96c6fe341af8d2a271ee22db3fe48d473e85c7a8
Sebastian A16 <sevarcas@gmail.com>
2026-08-19 15:27:34 -0500

    editprofilescreen: added topbar

--- project/app/src/main/java/com/example/kloth/ui/screens/editProfile/EditProfileScreen.kt
+++ project/app/src/main/java/com/example/kloth/ui/screens/editProfile/EditProfileScreen.kt
@@ -1,13 +1,20 @@
 package com.example.kloth.ui.screens.editProfile
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.kloth.ui.screens.editProfile.components.EditProfileTopBar
 
 @Composable
 fun EditProfileScreen(
     modifier: Modifier = Modifier
 ) {
-    EditProfileScreenContent(
-        modifier = modifier
-    )
+    Column(modifier = modifier.fillMaxSize()) {
+        EditProfileTopBar()
+        EditProfileScreenContent(
+            modifier = Modifier.weight(1f)
+        )
+    }
 }
+
